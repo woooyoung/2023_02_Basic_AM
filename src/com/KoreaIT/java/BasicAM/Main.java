9b8f83bac581038f974be60f21a72773acc892ee
woooyoung <axdsw121@gmail.com>
2023-02-01 15:50:21 +0900

    명령어 추가중(article write 구현중)

--- src/com/KoreaIT/java/BasicAM/Main.java
+++ src/com/KoreaIT/java/BasicAM/Main.java
@@ -12,8 +12,24 @@ public class Main {
 			System.out.printf("명령어 ) ");
 			String command = sc.nextLine();
 
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
 			if (command.equals("system exit")) {
 				break;
+			}
+
+			if (command.equals("article list")) {
+				System.out.println("게시글이 없습니다");
+			} else if (command.equals("article write")) {
+				System.out.println("제목 : ");
+				System.out.println("내용 : ");
+			}
+
+			else {
+				System.out.println("존재하지 않는 명령어입니다");
 			}
 
 		}
