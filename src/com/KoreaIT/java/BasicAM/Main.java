22c447b2608d9436e8abb1aa67de368005691f7d
woooyoung <axdsw121@gmail.com>
2023-02-02 13:42:29 +0900

    게시물 작성 기능 구현, article list 시 게시물의 유무 판단

--- src/com/KoreaIT/java/BasicAM/Main.java
+++ src/com/KoreaIT/java/BasicAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.BasicAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +10,12 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
 		while (true) {
+
 			System.out.printf("명령어 ) ");
 			String command = sc.nextLine();
 
@@ -22,10 +29,27 @@ public class Main {
 			}
 
 			if (command.equals("article list")) {
-				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				} else {
+					System.out.println("있던데?");
+				}
 			} else if (command.equals("article write")) {
-				System.out.println("제목 : ");
-				System.out.println("내용 : ");
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+//				System.out.printf("title : %s, body : %s\n", title, body);
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번 글이 생성 되었습니다\n", id);
+				lastArticleId++;
+
 			}
 
 			else {
@@ -37,5 +61,17 @@ public class Main {
 		System.out.println("==프로그램 끝==");
 
 		sc.close();
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
